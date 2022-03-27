f43be5e5cf29eb4ea75ac2e32f3a5f1be6f03bf2
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-03-27 10:27:47 +0300

    added Taco class to domain

---
+++ src/main/java/com/home/tacocloud/domain/Taco.java
@@ -0,0 +1,13 @@
+package com.home.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
