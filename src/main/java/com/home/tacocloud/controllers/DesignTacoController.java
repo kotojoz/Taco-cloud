6e1bb336b999a697b23e2effe48977bd63cd8099
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-03-27 12:58:32 +0300

    added to Design Taco Controller class POST method

--- src/main/java/com/home/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/home/tacocloud/controllers/DesignTacoController.java
@@ -7,10 +7,7 @@ import com.home.tacocloud.domain.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -56,6 +53,13 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
