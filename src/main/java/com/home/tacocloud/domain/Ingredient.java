584c58e283db7e7c3d6d30e1fff8da2db1548c5b
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-03-27 10:26:30 +0300

    added Ingredient class to domain

---
+++ src/main/java/com/home/tacocloud/domain/Ingredient.java
@@ -0,0 +1,15 @@
+package com.home.tacocloud.domain;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    private enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
