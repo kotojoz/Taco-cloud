75784271051cf78c74c50510896141e6adbdc261
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-04-04 18:41:48 +0300

    added to Security Configuration User Details Service

--- src/main/java/com/home/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/home/tacocloud/security/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.home.tacocloud.security;
 
+import com.home.tacocloud.domain.User;
+import com.home.tacocloud.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +15,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user!=null){
+                return user;
+            }
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 }
