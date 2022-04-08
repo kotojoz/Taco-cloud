06408c9e93ae6e7a25d84f0ebb550fd29ef2e134
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-04-08 18:45:45 +0300

    added Security Filter Chain

--- src/main/java/com/home/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/home/tacocloud/security/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.home.tacocloud.domain.User;
 import com.home.tacocloud.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +28,19 @@ public class SecurityConfig {
             }
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.authorizeRequests()
+                .antMatchers("/design", "/orders").hasRole("USER")
+                .antMatchers("/", "/**").permitAll()
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+
+                .and()
+                .build();
     }
 }
