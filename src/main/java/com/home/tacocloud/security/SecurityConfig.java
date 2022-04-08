0b54c2873dbdac64df6466e580c95d769e884ff3
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-04-08 19:05:42 +0300

    small fixes

--- src/main/java/com/home/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/home/tacocloud/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
 
                 .and()
                 .build();
