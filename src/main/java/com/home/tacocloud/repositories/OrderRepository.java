6149a90d0166217ab9b73aff88561afa4555a140
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-04-18 20:43:13 +0300

    added to Repository method which return all user`s orders

--- src/main/java/com/home/tacocloud/repositories/OrderRepository.java
+++ src/main/java/com/home/tacocloud/repositories/OrderRepository.java
@@ -1,7 +1,12 @@
 package com.home.tacocloud.repositories;
 
 import com.home.tacocloud.domain.TacoOrder;
+import com.home.tacocloud.domain.User;
+import org.hibernate.criterion.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<Order> findByUserOrderByPlacedAtDesc(User user);
 }
